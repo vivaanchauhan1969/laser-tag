083094e2e2fe5316095bc13a544387106427d08c
vivaanchauhan1969 <141391553+vivaanchauhan1969@users.noreply.github.com>
2024-08-07 02:35:17 -0500

    Update gun.lua

--- gun.lua
+++ gun.lua
@@ -171,3 +171,62 @@ Respawndant:FindFirstChild("Torso"):MakeJoints();
 Adam:remove();
 end;
 AdamHumanoid.Died:connect(Respawn);
+sp=script.Parent
+lastattack=0
+nextrandom=0
+nextsound=0
+nextjump=0
+chasing=false
+
+variance=4
+
+damage=50
+attackrange=4.5
+sightrange=999--60
+runspeed=40
+wonderspeed=8
+healthregen=false
+colors={"Sand red","Dusty Rose","Medium blue","Sand blue","Lavender","Earth green","Brown","Medium stone grey","Brick yellow"}
+
+function raycast(spos,vec,currentdist)
+	local hit2,pos2=game.Workspace:FindPartOnRay(Ray.new(spos+(vec*.01),vec*currentdist),script.Parent)
+	if hit2~=nil and pos2 then
+		if hit2.Parent==script.Parent and hit2.Transparency>=.8 or hit2.Name=="Handle" or string.sub(hit2.Name,1,6)=="Effect" or hit2.Parent:IsA("Hat") or hit2.Parent:IsA("Tool") or (hit2.Parent:FindFirstChild("Humanoid") and hit2.Parent:FindFirstChild("TEAM") and hit2.Parent:FindFirstChild("TEAM").Value == script.Parent.TEAM.Value) or (not hit2.Parent:FindFirstChild("Humanoid") and hit2.CanCollide==false) then
+			local currentdist=currentdist-(pos2-spos).magnitude
+			return raycast(pos2,vec,currentdist)
+		end
+	end
+	return hit2,pos2
+end
+
+function waitForChild(parent,childName)
+	local child=parent:findFirstChild(childName)
+	if child then return child end
+	while true do
+		child=parent.ChildAdded:wait()
+		if child.Name==childName then return child end
+	end
+end
+
+local Torso=waitForChild(sp,"Torso")
+local Head=waitForChild(sp,"Head")
+local RightShoulder=waitForChild(Torso,"Right Shoulder")
+local LeftShoulder=waitForChild(Torso,"Left Shoulder")
+local RightHip=waitForChild(Torso,"Right Hip")
+local LeftHip=waitForChild(Torso,"Left Hip")
+local Neck=waitForChild(Torso,"Tail")
+local Humanoid=waitForChild(sp,"Humanoid")
+local BodyColors=waitForChild(sp,"Body Colors")
+local pose="Standing"
+local hitsound=waitForChild(Head,"Bite Bark")
+local BARKING=waitForChild(Head,"Seal Barking")
+
+
+local sounds={
+	waitForChild(Torso,"GroanSound"),
+	waitForChild(Torso,"RawrSound")
+
+
+if healthregen then
+	local regenscript=waitForChild(sp,"HealthRegenerationScript")
+	regenscript.Disabled=false
