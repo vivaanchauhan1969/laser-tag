da0724d2d60084e8901125d4668adcdc197de672
vivaanchauhan1969 <141391553+vivaanchauhan1969@users.noreply.github.com>
2024-08-07 02:36:41 -0500

    Update gun.lua

--- gun.lua
+++ gun.lua
@@ -312,3 +312,81 @@ function animate(time)
 		moveJump()
 		return
 	end
+	if (pose == "FreeFall") then
+		moveFreeFall()
+		return
+	end
+	if (pose == "Seated") then
+		moveSit()
+		return
+	end
+	local climbFudge = 0
+	if (pose == "Running") then
+		RightShoulder.MaxVelocity = 0.15
+		LeftShoulder.MaxVelocity = 0.15
+		amplitude = 1
+		frequency = 9
+	elseif (pose == "Climbing") then
+		RightShoulder.MaxVelocity = 0.5 
+		LeftShoulder.MaxVelocity = 0.5
+		amplitude = 1
+		frequency = 9
+		climbFudge = 3.14
+	else
+		amplitude = 0.1
+		frequency = 1
+	end
+	desiredAngle = amplitude * math.sin(time*frequency)
+	if not chasing and frequency==9 then
+		frequency=4
+	end
+	if chasing then
+		[RightShoulder.DesiredAngle=math.pi/2
+		LeftShoulder.DesiredAngle=-math.pi/2
+		RightHip.DesiredAngle=-desiredAngle*2
+		LeftHip.DesiredAngle=-desiredAngle*2]]
+	else
+		RightShoulder.DesiredAngle=desiredAngle + climbFudge
+		LeftShoulder.DesiredAngle=desiredAngle - climbFudge
+		RightHip.DesiredAngle=-desiredAngle
+		LeftHip.DesiredAngle=-desiredAngle
+	end
+end
+
+
+function attack(time,attackpos)
+	if time-lastattack>=0.25 then
+		local hit,pos=raycast(Torso.Position,(attackpos-Torso.Position).unit,attackrange)
+		if hit and hit.Parent~=nil then
+			local h=hit.Parent:FindFirstChild("Humanoid")
+			local TEAM=hit.Parent:FindFirstChild("TEAM")
+			if h and TEAM and TEAM.Value~=sp.TEAM.Value then
+				local creator=sp:FindFirstChild("creator")
+				if creator then
+					if creator.Value~=nil then
+						if creator.Value~=game.Players:GetPlayerFromCharacter(h.Parent) then
+							for i,oldtag in ipairs(h:GetChildren()) do
+								if oldtag.Name=="creator" then
+									oldtag:remove()
+								end
+							end
+							creator:clone().Parent=h
+						else
+							return
+						end
+					end
+				end
+				hitsound.Volume=1
+				hitsound.Pitch=.75+(math.random()*.5)
+				hitsound:Play()
+				wait(0.15)
+				h:TakeDamage(damage)
+					if RightShoulder and LeftShoulder then
+					RightShoulder.CurrentAngle=0
+					LeftShoulder.CurrentAngle=0
+				end]]
+			end
+		end
+		lastattack=time
+	end
+end
