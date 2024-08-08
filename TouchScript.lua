9f7f53962e0ef0560abbd95e9980932aa4e0ab7c
vivaanchauhan1969 <141391553+vivaanchauhan1969@users.noreply.github.com>
2024-08-08 12:02:37 -0500

    Update TouchScript.lua

--- TouchScript.lua
+++ TouchScript.lua
@@ -33,3 +33,35 @@ function DoorOpen()
 end
 
 Click.MouseClick:Connect(DoorOpen)
+ local Toggle = false
+
+script.Parent.ClickDetector.MouseClick:Connect(function()
+	if Toggle == false then
+		Toggle = true
+		script.Parent.A.Motor.DesiredAngle = 1.5
+	else
+		Toggle = false
+		script.Parent.A.Motor.DesiredAngle = 0
+	end
+end)
+script.Parent.Locked.Changed:Connect(function()
+	if script.Parent.Locked.Value == false then
+		script.Parent.One.Click.MaxActivationDistance = 10
+		script.Parent.One.Script.Disabled = false
+		script.Parent.Two.Click.MaxActivationDistance = 10
+		script.Parent.Two.Script.Disabled = false
+		script.Parent.Three.Click.MaxActivationDistance = 10
+		script.Parent.Three.Script.Disabled = false
+		script.Parent.Four.Click.MaxActivationDistance = 10
+		script.Parent.Four.Script.Disabled = false
+	else
+		script.Parent.One.Click.MaxActivationDistance = 0
+		script.Parent.One.Script.Disabled = true
+		script.Parent.Two.Click.MaxActivationDistance = 0
+		script.Parent.Two.Script.Disabled = true
+		script.Parent.Three.Click.MaxActivationDistance = 0
+		script.Parent.Three.Script.Disabled = true
+		script.Parent.Four.Click.MaxActivationDistance = 0
+		script.Parent.Four.Script.Disabled = true
+	end
+end)
