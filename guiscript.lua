c23805f011f6d611357f0681e90a21b43295a0b1
vivaanchauhan1969 <141391553+vivaanchauhan1969@users.noreply.github.com>
2024-08-09 16:02:33 -0500

    Update guiscript.lua

--- guiscript.lua
+++ guiscript.lua
@@ -209,3 +209,54 @@ end
 	randomsound.Pitch=.5+(.5*math.random())
 	randomsound:Play()
 end]]
+				
+function tween(obj, dur, cmd)
+	game:GetService('TweenService'):Create(obj, dur, cmd):Play()
+end
+
+workspace.Camera.CameraType = Enum.CameraType.Scriptable
+				local One = script.One.Value
+local Two = script.Two.Value
+local Three = script.Three.Value
+local Speed = 200
+
+script.Parent.Parent.Flap.Changed:Connect(function()
+	if script.Parent.Parent.Flap.Value == 0 then
+		Speed = 200
+	elseif script.Parent.Parent.Flap.Value == 1 then
+		Speed = 180
+	elseif script.Parent.Parent.Flap.Value == 2 then
+		Speed = 170
+	elseif script.Parent.Parent.Flap.Value == 3 then
+		Speed = 160
+	elseif script.Parent.Parent.Flap.Value == 4 then
+		Speed = 150
+	end
+end)
+
+function SmokeOne()
+	if One.Velocity.Magnitude > Speed then
+		One.Smoke:Emit(100)
+		One.Touchdown:Play()
+	end
+end
+
+function SmokeTwo()
+	if Two.Velocity.Magnitude > Speed then
+		Two.Smoke:Emit(100)
+		Two.Touchdown:Play()
+	end
+end
+
+function SmokeThree()
+	if Three.Velocity.Magnitude > Speed then
+		Three.Smoke:Emit(100)
+		Three.Touchdown:Play()
+	end
+end
+
+
+One.Touched:Connect(SmokeOne)
+Two.Touched:Connect(SmokeTwo)
+Three.Touched:Connect(SmokeThree)
+tween(workspace.Camera, TweenInfo.new(1), {CFrame = workspace.MenuCam.CFrame})
