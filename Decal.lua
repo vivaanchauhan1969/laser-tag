03b0251f51e2f2c8ab7fec1b570fc17eeee26e29
vivaanchauhan1969 <141391553+vivaanchauhan1969@users.noreply.github.com>
2024-08-09 23:05:34 -0500

    Update Decal.lua

--- Decal.lua
+++ Decal.lua
@@ -39,3 +39,37 @@ function animate(time)
 	if (pose == "Seated") then
 		moveSit()
 		return
+	end
+	local climbFudge = 0
+	if (pose == "Running") then
+		RightShoulder.MaxVelocity = 0.15
+		LeftShoulder.MaxVelocity = 0.15
+		amplitude = 1
+		frequency = 9
+	elseif (pose == "Climbing") then
+		RightShoulder.MaxVelocity = 0.5 
+		LeftShoulder.MaxVelocity = 0.5
+		amplitude = 1
+		frequency = 9
+		climbFudge = 3.14
+	else
+		amplitude = 0.1
+		frequency = 1
+	end
+	desiredAngle = amplitude * math.sin(time*frequency)
+	if not chasing and frequency==9 then
+		frequency=4
+	end
+	if chasing then
+		[RightShoulder.DesiredAngle=math.pi/2
+		LeftShoulder.DesiredAngle=-math.pi/2
+		RightHip.DesiredAngle=-desiredAngle*2
+		LeftHip.DesiredAngle=-desiredAngle*2]]
+	else
+		RightShoulder.DesiredAngle=desiredAngle + climbFudge
+		LeftShoulder.DesiredAngle=desiredAngle - climbFudge
+		RightHip.DesiredAngle=-desiredAngle
+		LeftHip.DesiredAngle=-desiredAngle
+	end
+end
+
